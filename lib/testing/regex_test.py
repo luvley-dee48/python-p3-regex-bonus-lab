78e956e7883b8d36e134defd4ae6d26e708a61a7
deborah <fabbydebby@gmail.com>
2024-06-14 17:04:31 +0300

    bonus-lab

--- lib/testing/regex_test.py
+++ lib/testing/regex_test.py
@@ -11,26 +11,33 @@ FINDALL_STRING = """
     Maybe today's just not my day.
     It's clobbering time!
 """
-
 class TestRegEx:
     '''my_regex in regex.py'''
 
     def test_matches_its_such_a_lovely_day(self):
         '''matches the string "It's such a lovely day today."'''
-        assert(my_regex.fullmatch("It's such a lovely day today."))
+        assert my_regex.fullmatch("It's such a lovely day today.")
 
     def test_matches_some_weather_were_having(self):
         '''matches the string "Some weather we're having today, huh?"'''
-        assert(my_regex.fullmatch("Some weather we're having today, huh?"))
+        assert my_regex.fullmatch("Some weather we're having today, huh?")
 
     def test_matches_maybe_todays_not_my_day(self):
         '''matches the string "Maybe today's just not my day."'''
-        assert(my_regex.fullmatch("Maybe today's just not my day."))
+        assert my_regex.fullmatch("Maybe today's just not my day.")
 
     def test_finds_all_matches(self):
         '''can be used to find these three strings and ONLY these three strings.'''
-        assert(my_regex.findall(FINDALL_STRING) == [
+        expected_matches = [
             "It's such a lovely day today.",
             "Some weather we're having today, huh?",
             "Maybe today's just not my day.",
-        ])
+        ]
+        
+        # Find all matches in FINDALL_STRING
+        actual_matches = my_regex.findall(FINDALL_STRING)
+        
+        # Strip whitespace and newline characters from each match
+        actual_matches = [sentence.strip() for sentence in actual_matches]
+        
+        assert actual_matches == expected_matches
